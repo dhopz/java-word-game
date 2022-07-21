b9835aa5a99f36fa668ae1686ab728324ef01b3b
dhopz <david_houpapa@hotmail.com>
2022-07-21 14:39:03 +0100

    adding mockito

--- app/src/main/java/game/WordChoser.java
+++ app/src/main/java/game/WordChoser.java
@@ -5,12 +5,12 @@ import java.util.Random;
 public class WordChoser {
   public static final String[] DICTIONARY = {"MAKERS", "CANDIES", "DEVELOPER", "LONDON"}; 
 
-
+  public static void main(String[] args) {    
+  }
+  
   public String getRandomWordFromDictionary() {
     Random rand = new Random();
     return DICTIONARY[rand.nextInt(DICTIONARY.length)];
   }
 
-  public static void main(String[] args) {    
-  }
 }
--- app/src/test/java/game/GameTest.java
+++ app/src/test/java/game/GameTest.java
@@ -1,11 +1,13 @@
 package game;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 
 public class GameTest {
   @Test public void testsGetsWordToGuess() {
+    String mockedWord = mock(String);
     Game game = new Game();
     assertEquals(game.getWordToGuess(), "M_____");
   }
